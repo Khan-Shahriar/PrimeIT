772e209bc2b11d0ef652311af0f48b68648e9dc5
Shahriar <149973081+Khan-Shahriar@users.noreply.github.com>
2026-09-21 05:28:12 +0600

    Section 26: add backend architecture foundations

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const systemRoutes = require("./system");
+const authRoutes = require("./auth");
+const memberRoutes = require("./members");
+const roleRoutes = require("./roles");
+
+const router = express.Router();
+
+router.use("/", systemRoutes);
+router.use("/auth", authRoutes);
+router.use("/members", memberRoutes);
+router.use("/roles", roleRoutes);
+
+module.exports = router;
