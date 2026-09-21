449512a66674c29bdad433888c63c85ea298e1f8
Shahriar <149973081+Khan-Shahriar@users.noreply.github.com>
2026-09-21 17:04:28 +0600

    Section 28: mount user role assignment API

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const systemRoutes = require("./system");
 const authRoutes = require("./auth");
 const memberRoutes = require("./members");
 const roleRoutes = require("./roles");
+const userRoleRoutes = require("./userRoles");
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/", systemRoutes);
 router.use("/auth", authRoutes);
 router.use("/members", memberRoutes);
 router.use("/roles", roleRoutes);
+router.use("/users", userRoleRoutes);
 
 module.exports = router;
